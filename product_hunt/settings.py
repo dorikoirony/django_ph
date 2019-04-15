49067146ffa2d9e410de35fc62f2a59cb74a31ea
nanoRIPE <1033188854@qq.com>
2019-04-15 16:25:03 +0800

    主页传入数据，详细页面创建与标题点击跳转

--- product_hunt/settings.py
+++ product_hunt/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -119,8 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL  = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 STATIC_DIRS = [
     os.path.join(BASE_DIR,'products/static/'),
 ]
+MEDIA_URL   = '/media/'
+MEDIA_ROOT  = os.path.join(BASE_DIR,'media/')
--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Product(models.Model):
+    """Model definition for Product."""
+    objects  = models.Manager()
+    title    = models.CharField(default='请输入标题',max_length=50)
+    intro    = models.TextField(default='请输入介绍')
+    url      = models.CharField(default='',max_length=100)
+    icon     = models.ImageField(default='default_icon.png',upload_to='images/')
+    image    = models.ImageField(default='default_image.png',upload_to='images/')
+    votes    = models.IntegerField(default=1)
+    pub_date = models.DateTimeField()
+    hunter   = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        """Unicode representation of Product."""
+        return self.title
